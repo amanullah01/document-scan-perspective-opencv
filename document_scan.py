bedbd03da039198d4635531f5297e41eac0eb13d
Amanullah Aman <amancse.aiub@gmail.com>
2024-06-01 14:44:49 +0600

    document scan done with wrap perspective

--- document_scan.py
+++ document_scan.py
@@ -1,33 +1,79 @@
 import cv2
 import numpy as np
 
+width = 940
+height = 680
 
-def image_processing(img):
-    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    imageCanny = cv2.Canny(gray_image, 450, 600)
 
+def preprocess_image(img):
+    image_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    image_canny = cv2.Canny(image_gray, 450, 600)
     kernel = np.ones((5, 5), np.uint8)
-    image_dilation = cv2.dilate(imageCanny, kernel, iterations=2)
+    image_dilation = cv2.dilate(image_canny, kernel, iterations=2)
     image_erosion = cv2.erode(image_dilation, kernel, iterations=1)
     return image_erosion
 
 
 def draw_contours(img):
     contours, hierarchy = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    maxarea = 0
+    biggest = np.array([])
     for cnt in contours:
-        cv2.drawContours(image_contours, cnt, -1, (0, 255, 0), 2)
-        return image_contours
+        area = cv2.contourArea(cnt)
+        print("Area", area)
+        if area > 5000:
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+            if area > maxarea and len(approx) == 4:
+                biggest = approx
+    print("Biggest", biggest)
+    print("Biggest Shape", biggest.shape)
+    cv2.drawContours(image_contours, biggest, -1, (255, 0, 0), 3)
+
+    return biggest
 
 
-image = cv2.imread('./images/documentscanner2.jpg')
-resized_image = cv2.resize(image, (500, 500))
-image_contours = resized_image.copy()
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4, 2))
+    myPointsNew = np.zeros((4, 1, 2), np.int32)
 
-processed_image = image_processing(resized_image)
-contoursImage = draw_contours(processed_image)
+    add = myPoints.sum(1)
+    print("add", add)
 
-cv2.imshow('image', resized_image)
-cv2.imshow('Processed image', processed_image)
-cv2.imshow('Contours image', contoursImage)
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+
+    print("New Points", myPointsNew)
+
+    return myPointsNew
+
+
+def warp_perspective(img, biggest):
+    biggest = reorder(biggest)
+    pts1 = np.float32(biggest)
+    pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    imgOutput = cv2.warpPerspective(img, matrix, (width, height))
+    imgCropped = imgOutput[0:imgOutput.shape[0] - 10, 20:imgOutput.shape[1] - 10]
+    imgCropped = cv2.resize(imgCropped, (width, height))
+    return imgCropped
+
+
+image = cv2.imread("./images/documentscanner2.jpg")
+#image_resize=cv2.resize(image, (640, 480))
+image_contours = image.copy()
+preprocessed_image = preprocess_image(image)
+
+biggest = draw_contours(preprocessed_image)
+
+imgWraped = warp_perspective(image, biggest)
+print("Output Image", imgWraped.shape)
+cv2.imshow("Input Image", image)
+# cv2.imshow("Preprocessed Image", preprocessed_image)
+cv2.imshow("Output Image", imgWraped)
 cv2.waitKey(0)
+
 cv2.destroyAllWindows()
